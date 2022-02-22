ad8de5ba45633ab01f7af7788eb428f3da6084c8
roman-pikulenkor <roman@opensilver.net>
2022-02-23 01:07:08 +1000

    RadSlider example (SelectionRangeEnabled)

---
+++ RadComboBoxTest/SliderFilterViewModel.cs
@@ -0,0 +1,161 @@
+﻿using System.ComponentModel;
+using Telerik.Windows.Data;
+using System;
+
+namespace RadComboBoxTest
+{
+    /// <summary>SliderFilterViewModel</summary>
+    public class SliderFilterViewModel : INotifyPropertyChanged
+    {
+        #region Public Events -------------------------------------------------------------------
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion Public Events -------------------------------------------------------------------
+        #region Private ReadOnly Members --------------------------------------------------------
+
+        private readonly string dataMemberName;
+        private readonly FilterDescriptorCollection targetCollection;
+        private readonly double min;
+        private readonly double max;
+
+        private readonly CompositeFilterDescriptor compositeFilter;
+        private readonly FilterDescriptor startFilter;
+        private readonly FilterDescriptor endFilter;
+        #endregion Private ReadOnly Members --------------------------------------------------------
+        #region Public Properties ---------------------------------------------------------------
+
+        public bool IsActive
+        {
+            get
+            {
+                if (this.compositeFilter != null)
+                {
+                    return this.targetCollection.Contains(this.compositeFilter);
+                }
+
+                return false;
+            }
+        }
+
+        public double Start
+        {
+            get { return Convert.ToDouble(this.startFilter.Value); }
+            set
+            {
+                if ((double)this.startFilter.Value != value)
+                {
+                    this.EnsureFilterIsAddedInTargetCollection();
+                    this.startFilter.Value = value;
+                    this.OnPropertyChanged("Start");
+                    this.OnPropertyChanged("IsActive");
+                }
+            }
+        }
+
+        public double End
+        {
+            get { return Convert.ToDouble(this.endFilter.Value); }
+            set
+            {
+                if ((double)this.endFilter.Value != value)
+                {
+                    this.EnsureFilterIsAddedInTargetCollection();
+                    this.endFilter.Value = value;
+                    this.OnPropertyChanged("End");
+                    this.OnPropertyChanged("IsActive");
+                }
+            }
+        }
+        #endregion Public Properties ---------------------------------------------------------------
+        #region Private Methods -----------------------------------------------------------------
+
+        private void EnsureFilterIsAddedInTargetCollection()
+        {
+            if (this.compositeFilter != null
+                && !this.targetCollection.Contains(this.compositeFilter))
+            {
+                this.targetCollection.Add(this.compositeFilter);
+            }
+        }
+        #endregion Private Methods -----------------------------------------------------------------
+        #region Constructors --------------------------------------------------------------------
+
+        public SliderFilterViewModel(string dataMemberName
+            , FilterDescriptorCollection targetCollection
+            , double min
+            , double max)
+        {
+            this.dataMemberName = dataMemberName;
+            this.targetCollection = targetCollection;
+            this.min = min;
+            this.max = max;
+
+            this.compositeFilter = new CompositeFilterDescriptor();
+
+            this.startFilter = new FilterDescriptor(this.dataMemberName
+                , FilterOperator.IsGreaterThanOrEqualTo
+                , this.min);
+            this.compositeFilter.FilterDescriptors.Add(this.startFilter);
+
+            this.endFilter = new FilterDescriptor(this.dataMemberName
+                , FilterOperator.IsLessThanOrEqualTo
+                , this.max);
+            this.compositeFilter.FilterDescriptors.Add(this.endFilter);
+        }
+
+        public SliderFilterViewModel(string dataMemberName
+            , FilterDescriptorCollection targetCollection
+            , int min
+            , int max)
+        {
+            this.dataMemberName = dataMemberName;
+            this.targetCollection = targetCollection;
+            this.min = min;
+            this.max = max;
+
+            this.compositeFilter = new CompositeFilterDescriptor();
+
+            this.startFilter = new FilterDescriptor(this.dataMemberName
+                , FilterOperator.IsGreaterThanOrEqualTo
+                , this.min);
+            this.compositeFilter.FilterDescriptors.Add(this.startFilter);
+
+            this.endFilter = new FilterDescriptor(this.dataMemberName
+                , FilterOperator.IsLessThanOrEqualTo
+                , this.max);
+            this.compositeFilter.FilterDescriptors.Add(this.endFilter);
+        }
+        #endregion Constructors --------------------------------------------------------------------
+        #region Public Methods ------------------------------------------------------------------
+
+        public void Clear()
+        {
+            if (this.compositeFilter != null)
+            {
+                this.targetCollection.Remove(this.compositeFilter);
+                this.startFilter.Value = this.min;
+                this.OnPropertyChanged("Start");
+                this.endFilter.Value = this.max;
+                this.OnPropertyChanged("End");
+            }
+
+            this.OnPropertyChanged("IsActive");
+        }
+        #endregion Public Methods ------------------------------------------------------------------
+        #region On Property Changed -------------------------------------------------------------
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            if (null != this.PropertyChanged)
+            {
+                this.PropertyChanged(this, args);
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            this.OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion On Property Changed -------------------------------------------------------------
+    }
+}
---
+++ RadComboBoxTest/SliderFilteringControl.xaml.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using Telerik.Windows.Controls;
+using Telerik.Windows.Controls.GridView;
+
+namespace RadComboBoxTest
+{
+    public partial class SliderFilteringControl : UserControl, IFilteringControl
+    {
+        #region Private Members -----------------------------------------------------------------
+
+        private GridViewBoundColumnBase column;
+        #endregion Private Members -----------------------------------------------------------------
+        #region Public Properties ---------------------------------------------------------------
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the filtering is active.
+        /// </summary>
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+
+        public double Minimum
+        {
+            get { return (double)this.slider.Minimum; }
+            set { this.slider.Minimum = value; }
+        }
+
+        public double Maximum
+        {
+            get { return (double)this.slider.Maximum; }
+            set { this.slider.Maximum = value; }
+        }
+
+        public double TickFrequency
+        {
+            get { return (double)this.slider.TickFrequency; }
+            set { this.slider.TickFrequency = value; }
+        }
+        #endregion Public Properties ---------------------------------------------------------------
+        #region Public Static ReadOnly Members --------------------------------------------------
+
+        /// <summary>
+        /// Identifies the <see cref="IsActive"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty IsActiveProperty =
+            DependencyProperty.Register(
+                "IsActive",
+                typeof(bool),
+                typeof(SliderFilteringControl),
+                new System.Windows.PropertyMetadata(false));
+        #endregion Public Static ReadOnly Members --------------------------------------------------
+        #region Constructors --------------------------------------------------------------------
+
+        public SliderFilteringControl()
+        {
+            InitializeComponent();
+        }
+        #endregion Constructors --------------------------------------------------------------------
+        #region Public Methods ------------------------------------------------------------------
+
+#if OPENSILVER
+        public void Prepare(GridViewColumn column)
+#else
+        public void Prepare(GridViewBoundColumnBase column)
+#endif
+        {
+            if (this.column != column)
+            {
+#if OPENSILVER
+#else
+        this.column = column;
+#endif
+                string dataMemberName = this.column.DataMemberBinding.Path.Path;
+
+                SliderFilterViewModel vm = new SliderFilterViewModel(dataMemberName
+                    , this.column.DataControl.FilterDescriptors
+                    , this.Minimum
+                    , this.Maximum);
+
+                Binding binding = new Binding("IsActive")
+                {
+                    Source = vm,
+                    Mode = BindingMode.OneWay
+                };
+                this.ClearValue(SliderFilteringControl.IsActiveProperty);
+                this.SetBinding(SliderFilteringControl.IsActiveProperty, binding);
+
+                this.DataContext = vm;
+            }
+        }
+
+        public void Clear()
+        {
+            SliderFilterViewModel vm = this.DataContext as SliderFilterViewModel;
+            if (null != vm)
+            {
+                vm.Clear();
+            }
+        }
+        #endregion Public Methods ------------------------------------------------------------------
+        #region Button Events -------------------------------------------------------------------
+
+        private void OnClear(object sender, RoutedEventArgs e)
+        {
+            Clear();
+        }
+        #endregion Button Events -------------------------------------------------------------------
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Console.WriteLine($"Value: {e.NewValue}");
+        }
+
+        private void slider_SelectionChanged(object sender, Telerik.Windows.RadRoutedEventArgs e)
+        {
+            Console.WriteLine($"Selection: {slider.SelectionStart} ~ {slider.SelectionEnd}");
+        }
+    }
+
+    public class SliderTickConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && value is double)
+            {
+                double dVal = (double)value;
+                return dVal.ToString("0.00");
+            }
+            return "";
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
